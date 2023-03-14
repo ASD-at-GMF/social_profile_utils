5c4b365a7f7f93662b27e2e8c4268ee2cb3b4a4b
Peter Benzoni <pbenzoni@gmfus.org>
2023-03-14 14:07:01 -0400

    Adding Test Text

--- main.py
+++ main.py
@@ -12,20 +12,11 @@ parser.add_argument('--csv_path', '-c', type=str, help='A CSV file with at least
 # Parse the command line arguments
 args = parser.parse_args()
 
-text = '''During the Two Sessions, the government is expected to set its GDP growth target for 2023, which will surely receive added global attention, as China's recent reopening has paved the way for a faster-than-expected recovery. What kind of economic goals can be expected? Watch this video to unbox it.
-
-Unboxing Two Sessions is a "box" of China's policy trends that we can expect to see at this year's Two Sessions, a key event on China's political calendar. This year's event will be the first one after the 20th National Congress of the Communist Party of China, marking a crucial year for marching toward Chinese modernization. In each episode, we choose an item and unbox it together.
-
-For more: https://news.cgtn.com/news/2023-02-28...
-
-Follow us on TikTok & Telegram
-Streaming: https://www.epic-media.tv
-TikTok: https://www.tiktok.com/epicmediachannel
-Telegram: https://t.me/EpicMediaOfficial     
-Ukraine's Got Talent on Facebook: https://www.facebook.com/UkraineGot
-Ukraine's Got Talent on Instagram: https://www.instagram.com/UkraineGot
-Subscribe to us on YouTube: https://goo.gl/lP12gA
-Download our APP on Apple Store (iOS): https://itunes.apple.com/us/app/cctvn'''
+text = '''TEST TEXT Lorem Ipsum
+TikTok: https://www.tiktok.com/test_test
+Telegram: https://t.me/Test     
+Facebook: https://www.facebook.com/TEST
+Instagram: https://www.instagram.com/@Test'''
 
 if(args.text):
     # Set the description variable
